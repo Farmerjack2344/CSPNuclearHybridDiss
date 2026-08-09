a0c52d9732d23ead94c376f262b411c4080e36e1
Nathaniel <nathanieljohnson@gmail.com>
2026-08-09 16:25:32 +0100

    Started building the connections

--- Ap1000.py
+++ Ap1000.py
@@ -1,17 +1,22 @@
 import components
 from tespy.networks import Network
 from tespy.components import (Compressor, Condenser, HeatExchanger, Turbine, Splitter,
-                              Merge, CycleCloser, Drum, Source, Sink)
+                              Merge, CycleCloser, Drum, Source, Sink, Pump)
 from tespy.components.power.generator import Generator
+from tespy.connections import Connection
+
+
 
 AP1000_plant = Network()
 #Components
 cycle_closer = CycleCloser("cycle closer")
 # Steam generator
-reactor_in = Source('Reactor In')
-reactor_out = Sink('Reactor Out')
+reactor_out = Source('Reactor Out')
+reactor_in = Sink('Reactor In')
 
 steam_generator = HeatExchanger('Steam Generator')
+
+pre_turbine_split = Splitter('Pre-Turbine Split', num_out=2)
 
 # HP Turbine block
 HP_turbine_stg_1 = Turbine('HP Turbine Stage 1')
@@ -61,6 +66,11 @@ generator = Generator('Generator')
 ## Cooling water
 cooling_water_in = Source('Cooling Water In')
 cooling_water_out = Sink('Cooling Water Out')
+cooling_pump = Pump('Cooling Pump')
+
+condenser = Condenser('Condenser')
+
+
 
 
 # LP heaters
@@ -69,23 +79,26 @@ cooling_water_out = Sink('Cooling Water Out')
 
 #HeatExchanger - 2 IN and 2 OUT
 #SimpleHeatExchanger - 1 IN and 1 OUT
+LP_feedwater_pump = Pump('LP Feedwater Pump')
 
 LP_feedwater_heater_stg1 = HeatExchanger('LP Feedwater Heater 1')
 LP_feedwater_heater_stg2 = HeatExchanger('LP Feedwater Heater 2')
 LP_feedwater_heater_stg3 = HeatExchanger('LP Feedwater Heater 3')
-
 LP_feedwater_heater_stg4 = HeatExchanger('LP Feedwater Heater 4')# Last stage is a normal 2 in and 2 out
 # The extra line is just to a pump
-
-
-# HP Heaters
-HP_feedwater_heater_stg1 = HeatExchanger('HP Feedwater Heater 1')
-HP_feedwater_heater_stg2 = HeatExchanger('HP Feedwater Heater 2')
-HP_feedwater_heater_stg3 = HeatExchanger('HP Feedwater Heater 3')
-HP_feedwater_heater_stg4 = HeatExchanger('HP Feedwater Heater 4')
-
+LP_heater_merge_stg1 = Merge('LP Heater Merge 1', num_in=2)
+LP_heater_merge_stg2 = Merge('LP Heater Merge 1', num_in=2)
+LP_heater_merge_stg3 = Merge('LP Heater Merge 1', num_in=2)
+LP_heater_merge_stg4 = Merge('LP Heater Merge 1', num_in=2)
 
 dearator = Merge('Dearator', num_in=3)
+
+# HP Heaters
+HP_feedwater_pump = Pump('HP Feedwater Pump')
+HP_feedwater_heater_stg1 = HeatExchanger('HP Feedwater Heater 1')
+HP_feedwater_heater_stg2 = HeatExchanger('HP Feedwater Heater 2')
+
+
 
 # Setup
 AP1000_plant.units.set_defaults(
@@ -98,27 +111,35 @@ AP1000_plant.units.set_defaults(
 )
 
 #Connections
-#1 Primary Loop: Reactor and NaK loop
+#c = Connection(Component A, "Component A's port", Component B, "Component B's port", label)
+#1/2 Primary Loop: Reactor and NaK loop and Steam generator
+## in1/out1 = hot side, in2/out2 = cold side
 
-
-
-#2 Steam generator
 #Take the heat input from the previous step and we kno the steam generator cold stream output
 #The cold stream output can be a design parameter
 
+#Coolant into the SG
+c1 =  Connection(reactor_out, "out1", steam_generator, "in1",label = "primary_in")
+#Coolant out of SG
+c2 =  Connection(steam_generator, "out1", reactor_in, "in1",label="primary out")
+
+#Working fluid into SG
+c_fw = Connection(HP_feedwater_heater_stg2, "out1", steam_generator, "in2", label="feedwater_in")
+
 
 #Seperate into 2 streams
-
-#3.1 One stream going to the turbines
-#Ouput will be the streams leaving the turbines
-
-# Those stream objects will go to the interstage
-# And stream objects will be used in the feed water heater
-
-#Power should be calculated here
+#Working fluid out of SG
+c3 =  Connection(steam_generator, "out2", pre_turbine_split, "in1",label="secondary out")
 
 
-#3.2 Other stream going to the interstage heater(This is where one of the configuration will interface)
+#3.1 One stream going to the stage 2 interstage heater
+
+c4 = Connection(pre_turbine_split,"out1", interstage_heater_2, "in2", label="Initial bleed")
+
+#3.2 On stream going to the turbine
+
+
+
 
 #4 Streams going to the next stage of turbines
 
