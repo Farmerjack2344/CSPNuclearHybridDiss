989313f933a57d480ede01f6a3f46ca1148de266
Nathaniel <nathanieljohnson@gmail.com>
2026-08-30 21:28:30 +0100

    Added FWH fixing numerical issues

--- AP1000V5.py
+++ AP1000V5.py
@@ -8,8 +8,8 @@ from MultistageTurbine import MultiStageExtractionTurbine
 
 from tespy.connections import Connection
 # create a network object with R134a as fluid
-my_plant = Network()
-my_plant.units.set_defaults(
+AP1000_plant = Network()
+AP1000_plant.units.set_defaults(
     temperature="K",
     pressure="Pa",
     pressure_difference="Pa",
@@ -29,13 +29,13 @@ cc = CycleCloser('cycle closer')
 steam_generator= SimpleHeatExchanger('steam generator')
 condenser = Condenser('main condenser')
 condenser_merge = Merge('condenser merge')
-HP_turbine = MultiStageExtractionTurbine('HP turbine',num_stages=2)
+HP_turbine = MultiStageExtractionTurbine('HP turbine',num_stages=3)
 
 
 LP_turbine = Turbine('LP turbine')
 
-HP_FWH_2 = HeatExchanger('HP FWH')
-HP_FWH_1 = HeatExchanger('HP FWH')
+HP_FWH_2 = HeatExchanger('HP FWH 2')
+HP_FWH_1 = HeatExchanger('HP FWH 1')
 HP_FWH_M = Merge('HP FWH merge')
 
 HP_FWH_valve_1 = Valve('HP FWH drain valve 1')
@@ -63,32 +63,28 @@ c6 = Connection(condenser, 'out1',
 c7 = Connection(condenser_merge, 'out1',
                 pump, 'in1')
 
-c8 = Connection(pump, 'out1',
-                HP_FWH_2, 'in2')
-#c8 = Connection(pump, "out1", HP_FWH_1, "in2")
+c8 = Connection(pump, "out1", HP_FWH_1, "in2")
 
-#c8 = Connection(HP_FWH_1, "out2", HP_FWH_2, "in2")
+c9 = Connection(HP_FWH_1, "out2", HP_FWH_2, "in2")
 
-#c8 = Connection(HP_FWH_valve_2, "out1", HP_FWH_M, "in1")
-#c8 = Connection(HP_turbine, "out3", HP_FWH_M, "in2")
+c10 = Connection(HP_FWH_valve_2, "out1", HP_FWH_M, "in1")
+c11 = Connection(HP_turbine, "out3", HP_FWH_M, "in2")
 
-#c8 = Connection(HP_FWH_M,"out1", HP_FWH_1,"in1")
-#C8 = Connection(HP_FWH_1, "out1", HP_valve_1, "in1")
+c12 = Connection(HP_FWH_M,"out1", HP_FWH_1,"in1")
+c13 = Connection(HP_FWH_1, "out1", HP_FWH_valve_1, "in1")
 
 
 
 
-c9 = Connection(HP_FWH_2, 'out2',
-                steam_generator, 'in1')
+c14 = Connection(HP_FWH_2, 'out2',
+                 steam_generator, 'in1')
 
-c10 = Connection(
+c15 = Connection(
     HP_FWH_2, 'out1',
     HP_FWH_valve_2, 'in1')
 
-c11 = Connection(
-    HP_FWH_valve_2, 'out1',
-    condenser_merge, 'in2')
-#c11 = Connection(HP_valve_1, "out1", condenser_merge, "in2")
+
+c16 = Connection(HP_FWH_valve_1, "out1", condenser_merge, "in2")
 
 c0 = Connection(steam_generator,
                 'out1', cc, 'in1')
@@ -97,21 +93,32 @@ c0 = Connection(steam_generator,
 c1_1 = Connection(cwso, 'out1', condenser, 'in2', label='11')
 c1_2 = Connection(condenser, 'out2', cwsi, 'in1', label='12')
 
-my_plant.add_conns(c1, c2, c3, c5,c6,c7,c8,c9,c10, c11, c0, c1_1, c1_2)
+AP1000_plant.add_conns(c1, c2, c3, c5,
+                   c6, c7, c8, c9,c10,c11,c12,c13,
+                    c14, c15, c16, c0, c1_1, c1_2)
 
 condenser.set_attr(pr1=1, pr2=0.98)
-steam_generator.set_attr(Q=1707e6)
-HP_turbine.set_attr(eta_s1=0.845,eta_s2=0.845)
+steam_generator.set_attr(
+    Q=1707e6,
+    pr=0.97)
+
+HP_turbine.set_attr(eta_s1=0.845,eta_s2=0.845,eta_s3=0.845)
 LP_turbine.set_attr(eta_s=0.845)
 
 HP_FWH_2.set_attr(
-    Q=137.85e6,
+    Q=-137.85e6,
     pr1=0.97,
     pr2=0.97
 
 )
 
-c3.set_attr(m=89.9,p=2.83e6)
+HP_FWH_1.set_attr(
+    #Q=-147.91e6,
+    pr1=0.97,
+    pr2=0.97
+)
+
+
 
 pump.set_attr(eta_s=0.804)
 
@@ -121,7 +128,44 @@ c1_1.set_attr(T=288.15, p=1.2e5, fluid=cooling_fluid)
 c1_2.set_attr(T=300.15)
 
 c1.set_attr(T=600, p=150e5, m=1886.91, fluid=working_fluid)
+
+# HP Turbine Outlets
 c2.set_attr(p=113e4)# HP turbine outlet
+c3.set_attr(m=89.9,p=2.83e6)# HP turbine outlet 2
+c11.set_attr(m=71.72,p=1.79e6)# HP turbine outlet 3
+
+# Pump outlet -> HP FWH 1 cold in
+c8.set_attr(m0=1886.91, h0=7.9e5)
+
+# HP FWH 1 cold out -> HP FWH 2 cold in
+c9.set_attr(m0=1725.29 , h0=1.05e6)
+
+# HP FWH merge out -> HP FWH 1 hot in
+c12.set_attr(m0=161.6, h0=8.1e5)   # 89.9 + 71.72 ≈ combined drain mass
+
 c5.set_attr(p=40500)# LP turbine outlet
-my_plant.solve(mode='design')
-my_plant.print_results()
+c13.set_attr(m0=161.6, h0=5.7e5, x0=0)
+c15.set_attr(m0=89.9, h0=8.1e5, x0=0)
+c16.set_attr(m0=161.6, h0=5.7e5)
+
+try:
+
+    result = AP1000_plant.solve(mode="design",block_solve=True, robust_relax=True, oscillation_damping=True, max_iter=300)
+    print(f"Conerged: {"True" if AP1000_plant.status == 0 else "False"}")
+    print("\n" * 5)
+    AP1000_plant.print_structural_analysis()
+    print("\n" * 5)
+    AP1000_plant.print_variables()
+    print("\n" * 5)
+    AP1000_plant.print_incidence_matrix()
+    print("\n" * 5)
+    AP1000_plant.print_equations_with_dependents()
+
+
+
+
+except Exception as e:
+    print(f"Error: {e}")
+    AP1000_plant.print_variables()
+    AP1000_plant.print_structural_analysis()
+    AP1000_plant.print_incidence_matrix()
